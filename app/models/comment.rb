af1b0a575d42f894ca448123425fea95b750f049
ash949 <hamzaashoor949@hotmail.com>
2017-11-04 21:54:43 +0200

    added validations on comments and posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :post, presence: true
+  validates :user, uniqueness: { scope: :post, message: ': you have already reviewed this post'}
+  validates :body, presence: { message: ': review cannot be empty' }
+  validates :rating, numericality: { only_integer: true, message: ": you haven't rated the post" }
 end
